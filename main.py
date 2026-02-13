7d79673e6618bad67752922397ffb47b193eff9f
haseokyung6 <haseokyung@naver.com>
2026-02-13 09:54:20 +0900

    초기 환경 세팅

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import koreanize_matplotlib
+import matplotlib.pyplot as plt
+from loguru import logger
+
+# Matplotlib 한글 설정
+plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] = False
+
+# Loguru 설정
+logger.add("file_{time}.log", rotation="500 MB")
+
 def main():
     print("Hello from webscraping-gemini!")
+    logger.info("메인 함수가 실행되었습니다.")
 
 
 if __name__ == "__main__":
